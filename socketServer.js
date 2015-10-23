add0ad831094762e4ae9dfd4d81631741f9eed55
Mark F. Ellis II <ellismarkf@gmail.com>
2015-10-23 15:27:57 -0500

    revert to top level index.html in server

--- socketServer.js
+++ socketServer.js
@@ -8,7 +8,7 @@ var _ = require('lodash');
 var port = process.env.PORT || 5000;
 app.use('/static', express.static(__dirname + "/dist"));
 app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname, './dist/index.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 app.get('/dist/nimrod-compiled.js', function(req, res){
 	res.sendFile(path.join(__dirname, '/dist/nimrod-compiled.js'));
