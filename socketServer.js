d99cea04d35406b57c4cebf22a4fa2c7833fe6ba
Mark F. Ellis II <ellismarkf@gmail.com>
2015-10-23 14:14:00 -0500

    update static server express; remove Procfile and outdated webpack config

--- socketServer.js
+++ socketServer.js
@@ -6,7 +6,7 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 var _ = require('lodash');
 var port = process.env.PORT || 5000;
-app.use(express.static(__dirname + "/dist"));
+app.use('/static', express.static(__dirname + "/dist"));
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
