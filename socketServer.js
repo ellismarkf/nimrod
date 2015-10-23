da39e6a0224c50a0f78d02a3add94788cec0255b
Mark F. Ellis II <ellismarkf@gmail.com>
2015-10-23 13:53:10 -0500

    update epxress statics location

--- socketServer.js
+++ socketServer.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + "/"));
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
-app.get('/static/nimrod.js', function(req, res){
+app.get('/dist/nimrod-compiled.js', function(req, res){
 	res.sendFile(path.join(__dirname, '/dist/nimrod-compiled.js'));
 });
 io.sockets.on('connection', function(socket){
