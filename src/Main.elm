44d5b4a33c8aad5c69cbae7d4f321ae27de11b65
Mark Ellis <ellismarkf@gmail.com>
2020-10-09 22:40:06 -0500

    feat: mostly working game for local two player

--- src/Main.elm
+++ src/Main.elm
@@ -13,8 +13,8 @@ rows : List Int
 rows = List.indexedMap (+) (List.range 1 4)
  
 sticks : Int  -> List String
-sticks row  = List.map (combine row) (spread row) 
-
+sticks row  = List.map (combine row) (spread row)
+--
 spread : Int -> List Int
 spread n = List.range 1 n
 
@@ -30,34 +30,92 @@ combine rowValue itemIndex = formatId rowValue itemIndex
 ids : Set String
 ids = Set.fromList (List.concat (List.map sticks rows))
 
+parseStickId : String -> Int
+parseStickId id = quickToInt (String.right 1 id)
+
+parseRowId : String -> String
+parseRowId id = String.left 1 id
+
+quickToInt : String -> Int
+quickToInt s = Maybe.withDefault 0 (String.toInt s)
+
+checkSelected : String -> String -> Bool
+checkSelected row id = row == (String.left 1 id)
+
+countRemainingSticksByRow : String -> Set String -> Int
+countRemainingSticksByRow key stickIds = Set.size (Set.filter (checkSelected (String.left 1 key)) (Set.remove key stickIds))
+
+checkLastMove : String -> Set String -> Bool
+checkLastMove key stickIds = countRemainingSticksByRow key stickIds == 0
+
 type alias Model =
   { sticks : Set String
   , players : List Int
   , activePlayer : Int
+  , lastClicked : String
   }
 init : Model
 init =
   { sticks = ids
-  , players = [1, 2]
-  , activePlayer = 1
+  , players = [0, 1]
+  , activePlayer = 0
+  , lastClicked = "0"
   }
-
 
 type  Msg
    = StickSelected String
+   | TurnEnded
+
+updateSticks : String -> Set String -> Set String
+updateSticks key stickIds =
+  if checkLastMove key stickIds
+  then stickIds
+  else Set.remove key stickIds
+
+toggleActivePlayer : Int -> Int
+toggleActivePlayer activePlayer = abs (activePlayer - 1)
+
+updateActivePlayer : String -> Model -> Int
+updateActivePlayer key model =
+  if (checkLastMove key model.sticks) && (checkValidClick key model)
+  then toggleActivePlayer model.activePlayer
+  else model.activePlayer
+
+checkValidClick : String -> Model -> Bool
+checkValidClick key model = model.lastClicked == "0" || (model.lastClicked == parseRowId key)
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     StickSelected key ->
-      { model | sticks = Set.remove key model.sticks }
+      { model
+        | sticks =
+            if (checkValidClick (Debug.log "key" key) model)
+            then Set.remove key model.sticks
+            else model.sticks
+        , activePlayer = updateActivePlayer key model
+        , lastClicked =
+            if (checkValidClick key model) && (checkLastMove key model.sticks) then "0"
+            else if checkValidClick key model then parseRowId key
+            else model.lastClicked
+      }
+    TurnEnded ->
+      { model | activePlayer = toggleActivePlayer model.activePlayer, lastClicked = "0" }
       
+checkHidden : String -> Set String -> Bool
+checkHidden key stickIds = not (Set.member key stickIds)
 
 stickEl : Set String -> Int -> Int -> Html Msg
 stickEl stickIds n r =
   span
     [ class (getStickClass stickIds (formatId n r))
-    , onClick (StickSelected (formatId n r))
+    , onClick
+      ( StickSelected
+        ( if (checkHidden (formatId n r) stickIds)
+          then "-1:-1"
+          else formatId n r
+        )
+      )
     ]
     []
 
@@ -68,7 +126,7 @@ playerButtonEl : Int -> Int -> Html Msg
 playerButtonEl activePlayerId playerId =
   button
     [disabled (not (playerId == activePlayerId))]
-    [text ("p" ++ String.fromInt playerId)]
+    [text ("p" ++ String.fromInt (playerId + 1))]
 
 view : Model -> Html Msg
 view model =
@@ -76,5 +134,6 @@ view model =
     (List.concat
       [ List.map (playerButtonEl model.activePlayer) model.players
       , List.map (rowEl model.sticks) rows
+      , [button [class "turn", onClick TurnEnded][text "End Turn"]]
       ]
     )
